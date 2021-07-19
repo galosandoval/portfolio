2dac4eeb88be1b114f14b5555961819a1e101d4d
Galo Sandoval <galo.sandoval@ibm.com>
2021-07-19 13:41:57 -0700

    adds media query to modal

--- react-portfolio/src/components/ProjectModal.jsx
+++ react-portfolio/src/components/ProjectModal.jsx
@@ -4,22 +4,22 @@ import Backdrop from "@material-ui/core/Backdrop";
 import { makeStyles } from "@material-ui/core/styles";
 import React, { useState } from "react";
 
-// const useStyles = makeStyles((theme) => ({
-//   modal: {
-//     display: "flex",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   paper: {
-//     backgroundColor: theme.palette.background.paper,
-//     border: "2px solid #000",
-//     boxShadow: theme.shadows[5],
-//     padding: theme.spacing(2, 4, 3),
-//   },
-// }));
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 export const ProjectModal = (props) => {
-  //   const classes = useStyles();
+    const classes = useStyles();
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
@@ -36,7 +36,7 @@ export const ProjectModal = (props) => {
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
-        // className={classes.modal}
+        className={classes.modal}
         open={open}
         onClose={handleClose}
         closeAfterTransition
@@ -45,10 +45,9 @@ export const ProjectModal = (props) => {
           timeout: 500,
         }}
       >
+        <Fade in={open}>
         <p>Hello world</p>
-        {/* <Fade in={open}>
-          <div><p>hello</p></div>
-        </Fade> */}
+        </Fade>
       </Modal>
     </div>
   );
