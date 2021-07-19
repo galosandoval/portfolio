774982c6301d7cae1b44c47d30d36ba611818aa1
Galo Sandoval <galo.sandoval@ibm.com>
2021-07-19 13:27:20 -0700

    adds modal component

---
+++ react-portfolio/src/components/ProjectModal.jsx
@@ -0,0 +1,55 @@
+import Modal from "@material-ui/core/Modal";
+import Fade from "@material-ui/core/Fade";
+import Backdrop from "@material-ui/core/Backdrop";
+import { makeStyles } from "@material-ui/core/styles";
+import React, { useState } from "react";
+
+// const useStyles = makeStyles((theme) => ({
+//   modal: {
+//     display: "flex",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   paper: {
+//     backgroundColor: theme.palette.background.paper,
+//     border: "2px solid #000",
+//     boxShadow: theme.shadows[5],
+//     padding: theme.spacing(2, 4, 3),
+//   },
+// }));
+
+export const ProjectModal = (props) => {
+  //   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div className="modal">
+        <div className="modal-content" onClick={handleOpen}>{props.data}</div>
+      
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        // className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <p>Hello world</p>
+        {/* <Fade in={open}>
+          <div><p>hello</p></div>
+        </Fade> */}
+      </Modal>
+    </div>
+  );
+};
--- react-portfolio/src/components/Services.jsx
+++ react-portfolio/src/components/Services.jsx
@@ -1,14 +1,80 @@
 import "./Components.css";
+import { ProjectModal } from "./ProjectModal";
 export const Services = () => {
+  const project1 = (
+      <div className="card">
+        <div className="box">
+          <div className="text">Project Manager</div>
+          <p>
+            Web app designed for users to keep track of the people on their
+            team, projects their team are working, and a users profile
+            information.
+          </p>
+          <p>Javascript || React || Styled Components</p>
+          <p>
+            - Frontend developer of project with a team of 3 other FE developers
+            and 4 Backend Developers
+          </p>
+          <p>
+            - Acted as the leader of the FE team and communicated with the
+            leader of the BE team to discuss any progress or problems
+          </p>
+        </div>
+      </div>
+  );
+  const project2 = (
+
+      <div className="card">
+        <div className="box">
+          <div className="text">Human Rights First</div>
+          <p>
+            A React web application that consolidates instances of police use of
+            force onto a map and graph of the US.
+          </p>
+          <p>Javascript || React || Victory Charts</p>
+          <p>
+            - Collaborated with a team of 1 other frontend developer and 3 data
+            scientists to rebuild a web application in just a few weeks.{" "}
+          </p>
+          <p>
+            - Responsible for creating a new graph to display data retrieved
+            from data scientists.
+          </p>
+          <p>
+            - Molded the way the 1000+ dataset would be implemented into the
+            app.
+          </p>
+        </div>
+      </div>
+  );
+  const project3 = (
+
+      <div className="card">
+        <div className="box">
+          <div className="text">Slack Clone</div>
+          <p>
+            This is a project I did on my own. I really enjoy the UI for slack
+            and I thought it would be good practice to see if I could recreate
+            it.
+          </p>
+          <p>React || Material UI || CSS || Redux</p>
+          <p>- Implemented Firebase for the database and deployment.</p>
+        </div>
+      </div>
+  );
   return (
     <section className="services" id="services">
       <div className="max-width">
         <h2 className="title">Portfolio</h2>
         <div className="serv-content">
-          <a
+          <ProjectModal data={project1} />
+          <ProjectModal data={project2} />
+          <ProjectModal data={project3} />
+          {/* <a
             target="blank"
             style={{ textDecoration: "none", color: "#fff" }}
             href="https://github.com/galosandoval/cooksysfinalFE"
+            className="a"
           >
             <div className="card">
               <div className="box">
@@ -75,8 +141,8 @@ export const Services = () => {
                 <p>React || Material UI || CSS || Redux</p>
                 <p>- Implemented Firebase for the database and deployment.</p>
               </div>
-            </div>
-          </a>
+            </div> */}
+          {/* </a> */}
         </div>
       </div>
     </section>
