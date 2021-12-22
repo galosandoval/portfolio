a3105779acb4e21aaee29fa2567dd572c6c8a70e
Galo Sandoval <galosan92@gmail.com>
2021-12-22 11:21:55 -0800

    implements emailjs-com in form and adds status handling

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -4,7 +4,13 @@ export const projects = [
   {
     name: "Listy",
     stack: ["React", "SCSS", "Node.js", "Express", "React-query", "sqlite3", "Knex"],
-    highlights: ["Carousel", "JWT", "Lazy Loading", "React-query instead of state management"],
+    highlights: [
+      "Carousel",
+      "JWT",
+      "Lazy Loading",
+      "React-query instead of state management",
+      "Tasks managed by Trello"
+    ],
     description:
       "Web App I made to save recipes and make lists for grocery shopping. The main feature of the app is it parses instructions and ingredients from your favorite recipes and saves them to your account.",
     video: listy,
@@ -14,7 +20,11 @@ export const projects = [
   {
     name: "4-7-8 Breathing Helper",
     stack: ["React", "JavaScript", "CSS"],
-    highlights: ["useEffect with timers", "Animation with keyframes"],
+    highlights: [
+      "useEffect with timers",
+      "Animation with keyframes",
+      "Saves user's preference in local storage"
+    ],
     description:
       "Breathing exercises can improve your well-being. With the 4-7-8 Breathing Helper, control your cravings and sleep faster. You'll find links to more information in the app.",
     video: listy,
@@ -24,22 +34,23 @@ export const projects = [
   },
   {
     name: "Portolio",
-    stack: ["React", "Styled-Components"],
-    highlights: ["Debounce", "Dark Mode", ""],
+    stack: ["React", "Styled-Components", "JavaScript"],
+    highlights: ["Debounce", "Dark Mode", "Planned using Figma"],
     description:
-      "Variety of small projects I've worked on from courses with my apprenticeship at IBM or a technology I wanted to learn on the side.",
+      "Made using figma to make wireframes, to plan out components, colors, and layout. Yes, it's the site you're currently on.",
     video: listy,
     web: null,
     github: "https://github.com/galosandoval"
   },
   {
     name: "Misc Projects",
-    stack: ["Java", "Docker", "SCSS", "Styled-Components", "Redux"],
-    highlights: [""],
+    stack: ["Java", "Docker", "SCSS", "Styled-Components", "Redux", "Angular", "Python"],
+    highlights: ["AWS"],
     description:
       "Variety of small projects I've worked on from courses with my apprenticeship at IBM or a technology I wanted to learn on the side.",
     video: listy,
     web: null,
-    github: "https://github.com/galosandoval"
+    github: "https://github.com/galosandoval",
+    reverse: true
   }
 ];
