7573d748f6ce013c211c6202ad78835665ef30b4
Galo Sandoval <galosan92@gmail.com>
2022-01-06 14:08:51 -0800

    edits html file and loading timer

--- src/components/Loading/Loading.styles.jsx
+++ src/components/Loading/Loading.styles.jsx
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components/macro";
+import styled from "styled-components/macro";
 
 export const StyledLoading = styled.div`
   background-color: ${({ theme }) => theme.body};
@@ -21,56 +21,6 @@ export const LoadingTitle = styled.h1`
   font-size: 4em;
 `;
 
-export const DotsLight = keyframes`
-  20% {
-    color: rgba(0,0,0,0);
-    text-shadow:
-      .25em 0 0 rgba(0,0,0,0),
-      .5em 0 0 rgba(0,0,0,0);
-  }
-  40% {
-    color: #eac234;
-    text-shadow:
-      .25em 0 0 rgba(0,0,0,0),
-      .5em 0 0 rgba(0,0,0,0);
-  }
-  60% {
-    text-shadow:
-      .25em 0 0 #eac234,
-      .5em 0 0 rgb(0,0,0,0);
-  }
-  100% {
-    color: #eac234;
-    text-shadow:
-      .25em 0 0 #eac234,
-      .5em 0 0 #eac234;}
-    `;
-
-export const DotsDark = keyframes`
-  20% {
-    color: rgba(0,0,0,0);
-    text-shadow:
-      .25em 0 0 rgba(0,0,0,0),
-      .5em 0 0 rgba(0,0,0,0);
-  }
-  40% {
-    color: #cbb1af;
-    text-shadow:
-      .25em 0 0 rgba(0,0,0,0),
-      .5em 0 0 rgba(0,0,0,0);
-  }
-  60% {
-    text-shadow:
-      .25em 0 0 #cbb1af,
-      .5em 0 0 rgb(0,0,0,0);
-  }
-  100% {
-    color: #cbb1af;
-    text-shadow:
-      .25em 0 0 #cbb1af,
-      .5em 0 0 #cbb1af;}
-    `;
-
 export const LoadingDots = styled.span`
-  animation: ${({ theme }) => theme.loadingScreen} 3s infinite;
+  animation: ${({ theme }) => theme.loadingScreen} 1.5s infinite;
 `;
